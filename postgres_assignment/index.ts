a08af102b8fdeaa632bf1c2d148c8c8cad5e66fc
DeepakDua-appinventiv <Deepak.Dua@appinventiv.com>
2023-07-17 19:08:26 +0530

    deepak

---
+++ postgres_assignment/index.ts
@@ -0,0 +1,211 @@
+import express, { Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { Sequelize, Model, DataTypes } from 'sequelize';
+import { Request } from 'express';
+// import { bcrypt } from 'bcrypt';
+// import bcrypt from "bcrypt"
+const app = express();
+const sequelize = new Sequelize('postgres', 'postgres', 'Deepak', {
+    host: 'localhost',
+    dialect: 'postgres'
+  });
+
+
+(async function (){
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+})()
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public password!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'users',
+  }
+);
+
+User.sync();
+console.log("The table for the User model was just (re)created!");
+
+app.use(express.json());
+
+app.post('/signup', async (req:Request, res:Response) => {
+  
+  const { username, password } = req.body;
+  const user = await User.create({ username, password }); 
+  const userId = user.id;
+  
+  // const token = jwt.sign({ userId }, 'secret');
+
+  res.send("sign up successful");
+});
+
+app.use(express.json());
+
+app.post('/login', async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // const result = await sequelize.query("SELECT * FROM users");
+    
+    const user:any = await User.findAll({ where: { username:username,password: password }, raw:true });
+    console.log("=======================>\n\n",user);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+  
+    // const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+    // if (!isPasswordValid) {
+    //   return res.status(401).send('Invalid password');
+    // }
+      const token = jwt.sign({ userId: user.id }, 'secret');
+      res.send({ token });
+  });
+
+class Post extends Model {
+ id!: number;
+ title!: string;
+ content!: string;
+ userId!: number;
+}
+
+Post.init(
+ {
+   post_id: {
+     type: DataTypes.INTEGER,
+     primaryKey: true,
+     autoIncrement: true,
+   },
+   title: {
+     type: DataTypes.STRING,
+     allowNull: false,
+   },
+   content: {
+     type: DataTypes.STRING,
+     allowNull: false,
+   },
+   userId: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+   },
+ },
+ {
+   sequelize, // Pass the Sequelize instance
+   tableName: 'posts', // Set the table name
+ //   timestamps: true, // Add timestamps (createdAt, updatedAt)
+ //   underscored: true, // Use snake_case for column names
+ }
+);
+Post.sync();
+
+app.post('/post', async (req: Request, res: Response) => {
+ // Check for validation errors
+ const { title, content, userId } = req.body;
+ const post = await Post.create({title,content,userId});
+ console.log("Post Created");
+
+ // Return a success response
+ res.status(201).json({ message: 'Post created successfully' });
+});
+
+class Comment extends Model {
+ public id!: number;
+ public postId!: number;
+ public userId!: number;
+ public content!: string;
+}
+
+Comment.init(
+ {
+   id: {
+     type: DataTypes.INTEGER,
+     autoIncrement: true,
+     primaryKey: true,
+   },
+   postId: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+   },
+   userId: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+   },
+   content: {
+     type: DataTypes.STRING,
+     allowNull: false,
+   },
+ },
+ {
+   sequelize,
+   tableName: 'Comments',
+ }
+);
+
+
+//   Comment.sync({ force: true });
+app.post('/comments', async (req: Request, res: Response) => {
+ try {
+   const { postId, userId, content } = req.body;
+
+   // Create the comment
+   const comment = await Comment.create({
+     postId,
+     userId,
+     content,
+   });
+
+   res.status(201).json(comment);
+ } catch (error) {
+   console.error('Error creating comment:', error);
+   res.status(500).json({ error: 'Failed to create comment' });
+ }
+});
+
+app.delete('/delete_comments', async (req: Request, res: Response) => {
+ try {
+   const { id } = req.body;
+
+   // Find the comment by ID
+   const comment = await Comment.findByPk(id);
+
+   // Check if the comment exists
+   if (!comment) {
+     return res.status(404).json({ error: 'Comment not found' });
+   }
+
+   // Delete the comment
+   await comment.destroy();
+
+   res.status(204).end();
+ } catch (error) {
+   console.error('Error removing comment:', error);
+   res.status(500).json({ error: 'Failed to remove comment' });
+ }
+});
+ 
+app.listen(3001, () => {
+  console.log('Server started on port 3001');
+});
